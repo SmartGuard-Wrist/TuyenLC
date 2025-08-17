d6ad34d33abc6f415136dfcd1656e101350d97cf
Lê Chí Tuyền <lctuyenvn@gmail.com>
2025-08-17 20:25:58 +0700

    code read battery voltage

---
+++ voltage_divider1.ino
@@ -0,0 +1,13 @@
+void setup()
+{
+	pinMode(A0, INPUT);
+  	Serial.begin(9600);
+}
+
+void loop()
+{
+  int value = analogRead(A0);
+  float voltage = value * 5.0 *2.0 / 1023;
+  Serial.println(voltage);
+  delay(100);
+}
